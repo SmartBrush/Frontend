637415158a785f071f3f31c15ddbde94fbde863d
phjlia2430 <phjlia2430@naver.com>
2025-10-15 20:23:00 +0900

    🐛 [Fix] navbar 수정

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,118 +1,36 @@
-// import { Outlet } from 'react-router-dom'
-// import Navbar from '../components/NavBar/NavBar'
-
-// const RootLayout = () => {
-//   return (
-//     <div className="min-h-screen w-full flex justify-center bg-[#313131] font-[Pretendard] overflow-hidden">
-//       <div className="w-full max-w-[400px] flex flex-col relative">
-//         {/* 스크롤 가능한 영역 */}
-//         <main
-//           className="overflow-y-auto bg-[#f5f5f5]"
-//           style={{ height: 'calc(100vh - 64px)' }}
-//         >
-//           <Outlet />
-//         </main>
-
-//         {/* 고정된 네비게이션 바 */}
-//         <div className="h-[64px] w-full shrink-0 bg-white sticky bottom-0 z-10">
-//           <Navbar />
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default RootLayout
-
-// import { Outlet } from 'react-router-dom'
-// import Navbar from '../components/NavBar/NavBar'
-
-// const RootLayout = () => {
-//   return (
-//     <div className="min-h-screen w-full flex justify-center bg-[#313131] font-[Pretendard]">
-//       <div className="w-full max-w-[400px] flex flex-col h-screen">
-//         {/* 스크롤 가능한 영역 */}
-//         <main className="flex-1 overflow-y-auto bg-[#f5f5f5]">
-//         <main className="flex-1 overflow-y-auto bg-white">
-//           <Outlet />
-//         </main>
-
-//         {/* 고정된 네비게이션 바 */}
-//         <div className="h-[64px] w-full shrink-0 bg-white">
-//           <Navbar />
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default RootLayout
-
-// import { Outlet, useLocation } from 'react-router-dom'
-// import Navbar from '../components/NavBar/NavBar'
-
-// const NAV_H = 64
-
-// const RootLayout = () => {
-//   const location = useLocation()
-
-//   // /question으로 시작하는 페이지에서는 NavBar 숨기기
-//   const isQuestionPage = location.pathname.startsWith('/question')
-
-//   return (
-//     <div className="min-h-screen w-full flex justify-center bg-black font-[Pretendard]">
-//       <div className="w-full max-w-[400px] mx-auto relative">
-//         <main
-//           className="overflow-y-auto bg-white"
-//           style={{
-//             minHeight: isQuestionPage ? '100vh' : `calc(100vh - ${NAV_H}px)`,
-//           }}
-//         >
-//           <Outlet />
-//         </main>
-
-//         {!isQuestionPage && (
-//           <div
-//             className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[400px] bg-white"
-//             style={{ height: NAV_H }}
-//           >
-//             <Navbar />
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default RootLayout
-
+// src/layouts/RootLayout.tsx
 import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../components/NavBar/NavBar'
 
-const RootLayout = () => {
-  const location = useLocation()
-  const pathname = location.pathname
+const NAV_H = 72
 
-  // question 페이지나 login 페이지에서는 NavBar 숨김
+const RootLayout = () => {
+  const { pathname } = useLocation()
   const hideNavbar =
     pathname.startsWith('/question') || pathname.startsWith('/login')
 
-  // / 일 때만 green, 나머지는 white
-  // const navbarBg = pathname === '/' ? 'bg-[rgba(182,232,178,0.5)]' : 'bg-white'
-
   return (
     <div className="min-h-screen w-full flex justify-center bg-[#313131] font-[Pretendard]">
-      {/* 앱 전체 박스 */}
-      <div className="w-full max-w-[400px] flex flex-col h-screen bg-white">
-        {/* 안쪽에서만 스크롤 */}
-        <main className="flex-1 overflow-y-auto bg-white">
+      <div className="w-full max-w-[400px] flex flex-col h-screen bg-white relative">
+        {/* 내부 스크롤 + NavBar 높이만큼 패딩 */}
+        <main
+          className="flex-1 overflow-y-auto bg-white"
+          style={{
+            paddingBottom: hideNavbar ? 0 : `calc(${NAV_H}px`,
+          }}
+        >
           <Outlet />
-          {!hideNavbar && (
-            <div className="h-[72px] w-full shrink-0">
-              <Navbar />
-            </div>
-          )}
         </main>
+
+        {/* 고정 네비게이션 바 (더미 div 제거) */}
+        {!hideNavbar && (
+          <div
+            className="fixed left-1/2 -translate-x-1/2 bottom-0 w-full max-w-[400px]"
+            style={{ height: NAV_H }}
+          >
+            <Navbar />
+          </div>
+        )}
       </div>
     </div>
   )
