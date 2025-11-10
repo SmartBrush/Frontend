2eb46f83c4aad94a54187b16f4c020e5785737a3
nadomola <siyun778@gmail.com>
2025-11-10 16:08:47 +0900

    :bug: 에러 수정
    에러 수정

--- src/apis/mbti.ts
+++ src/apis/mbti.ts
@@ -10,14 +10,14 @@ export type ScalpMbtiSummary = {
 
 // 한국어 라벨 → 카드 키
 const MBTI_KO_MAP: Record<string, MbtiCardKey> = {
-  '트러블 폭풍형': 'oily_trouble',
   '지성 민감형': 'oily_sensitive',
-  '지성 비듬형': 'oily_dandruff',
-  '깔끔 지성형': 'clean_oily',
-  '깐깐 지성형': 'clean_oily',
-  '건조 트러블형': 'dry_trouble',
-  '민감 건조형': 'dry_sensitive',
-  '건조 비듬형': 'dry_scaling',
+  '지성 비듬형': 'oily_scaling',
+  '지성 깔끔형': 'oily_clean',
+  '지성 트러블형': 'oily_trouble',
+  '건성 민감형': 'dry_sensitive',
+  '건성 비듬형': 'dry_scaling',
+  '건성 깔끔형': 'dry_clean',
+  '건성 트러블형': 'dry_trouble',
   밸런스형: 'balanced',
 }
 
